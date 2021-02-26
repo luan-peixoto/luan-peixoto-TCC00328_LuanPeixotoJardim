9b79adde0e6800c7d36873fb18eeec66344cd970
Luiz Leme <lapaesleme@ic.uff.br>
2021-02-26 15:10:02 -0300

    no message

--- src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/tutorialOOCont/LuanPeixotoJardim/SistemaAcademico.java
+++ src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/tutorialOOCont/LuanPeixotoJardim/SistemaAcademico.java
@@ -1,47 +1,41 @@
 package uff.ic.lleme.tcc00328.s20202.exercicios.tutorialOOCont.LuanPeixotoJardim;
 
-import uff.ic.lleme.tcc00328.s20202.exercicios.tutorialOO.LuanPeixotoJardim.*;
-import java.io.File;  
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner; 
-import java.util.concurrent.ThreadLocalRandom;
-import java.lang.Math;
+import java.util.Scanner;
 
 public class SistemaAcademico {
-    
+
     public static Aluno[] alunos = new Aluno[10];
     public static Disciplina[] disciplinas = new Disciplina[10];
     public static Inscricao[] inscricoes = new Inscricao[10];
     public static Turma[] turmas = new Turma[20];
-    public static String alunos_texto = "src\\main\\java\\uff\\ic\\lleme\\tcc00328\\s20202\\exercicios\\"
-            + "tutorialOOCont\\LuanPeixotoJardim\\alunos.txt";
-    public static String disciplinas_texto = "src\\main\\java\\uff\\ic\\lleme\\tcc00328\\s20202\\exercicios\\"
-            + "tutorialOOCont\\LuanPeixotoJardim\\disciplinas.txt";
-    public static String inscricoes_texto = "src\\main\\java\\uff\\ic\\lleme\\tcc00328\\s20202\\exercicios\\"
-            + "tutorialOOCont\\LuanPeixotoJardim\\inscricoes.txt";
-    public static String turmas_texto = "src\\main\\java\\uff\\ic\\lleme\\tcc00328\\s20202\\exercicios\\"
-            + "tutorialOOCont\\LuanPeixotoJardim\\turmas.txt";
-    
+    public static String alunos_texto = "src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/"
+            + "tutorialOOCont/LuanPeixotoJardim/alunos.txt";
+    public static String disciplinas_texto = "src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/"
+            + "tutorialOOCont/LuanPeixotoJardim/disciplinas.txt";
+    public static String inscricoes_texto = "src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/"
+            + "tutorialOOCont/LuanPeixotoJardim/inscricoes.txt";
+    public static String turmas_texto = "src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/"
+            + "tutorialOOCont/LuanPeixotoJardim/turmas.txt";
+
     public static void main(String[] args) {
-        
+
         carregarAlunos(alunos_texto);
-        carregarDisciplinas(disciplinas_texto);    
+        carregarDisciplinas(disciplinas_texto);
         carregarTurmas(turmas_texto);
         carregarInscricoes(inscricoes_texto);
         informarNotasDeAlunos();
-        
-        
+
         for (int i = 0; i < 10; i++) {
-            if (alunos[i] == null) {
+            if (alunos[i] == null)
                 break;
-            }
             imprimirInfoAluno(alunos[i]);
-            
+
         }
-        
-        
+
     }
-    
+
     public static void carregarAlunos(String nomeArquivo) {
         int atual = 0;
         try {
@@ -58,9 +52,9 @@ public class SistemaAcademico {
         } catch (FileNotFoundException e) {
             System.out.println("Deu ruim");
             e.printStackTrace();
-        }    
+        }
     }
-    
+
     public static void carregarDisciplinas(String nomeArquivo) {
         int atual = 0;
         try {
@@ -79,7 +73,7 @@ public class SistemaAcademico {
             e.printStackTrace();
         }
     }
-    
+
     public static void carregarTurmas(String nomeArquivo) {
         int atual = 0;
         try {
@@ -90,13 +84,12 @@ public class SistemaAcademico {
                 String[] atributos = linha.split("#");
                 Disciplina disciplina = null;
                 for (int i = 0; i < 10; i++) {
-                    if (disciplinas[i] == null) { // limite do vetor
+                    if (disciplinas[i] == null) // limite do vetor
                         break;
-                    }
                     if (disciplinas[i].codigo.equals(atributos[1])) {
                         disciplina = disciplinas[i];
                         break;
-                    } 
+                    }
                 }
                 Turma turma = new Turma(atributos[0], disciplina, atributos[2], atributos[3]);
                 turmas[atual] = turma;
@@ -108,8 +101,7 @@ public class SistemaAcademico {
             e.printStackTrace();
         }
     }
-    
-    
+
     public static void carregarInscricoes(String nomeArquivo) {
         int atual = 0;
         int i;
@@ -122,20 +114,18 @@ public class SistemaAcademico {
                 String nome_aluno = null;
                 Turma turma = null;
                 for (i = 0; i < 10; i++) {
-                    if (alunos[i] == null) { // limite do vetor
+                    if (alunos[i] == null) // limite do vetor
                         break;
-                    }
                     if (alunos[i].matricula.equals(atributos[1])) {
                         nome_aluno = alunos[i].nome;
                         break;
-                    } 
+                    }
                 }
                 for (i = 0; i < 20; i++) {
-                    if (turmas[i] == null) { // limite do vetor
+                    if (turmas[i] == null) // limite do vetor
                         break;
-                    }
-                    if (turmas[i].codigo.equals(atributos[4]) && 
-                            turmas[i].disciplina.codigo.equals(atributos[0])) {
+                    if (turmas[i].codigo.equals(atributos[4])
+                            && turmas[i].disciplina.codigo.equals(atributos[0])) {
                         turma = turmas[i];
                         break;
                     }
@@ -148,73 +138,60 @@ public class SistemaAcademico {
         } catch (FileNotFoundException e) {
             System.out.println("Deu ruim");
             e.printStackTrace();
-        }    
+        }
     }
-    
-    
+
     public static void informarNotasDeAlunos() {
         int i;
         int j;
         int k;
         int l;
         for (i = 0; i < 10; i++) {
-            if (inscricoes[i] == null) { // limite do vetor
+            if (inscricoes[i] == null) // limite do vetor
                 break;
-            }
-            for (j = 0; j < 3; j++) {
-                inscricoes[i].notas[j] = (float)(0 + Math.random() * (10 - 0));
-            }
+            for (j = 0; j < 3; j++)
+                inscricoes[i].notas[j] = (float) (0 + Math.random() * (10 - 0));
             for (k = 0; k < 10; k++) {
-                if (alunos[k] == null) { // limite do vetor
-                break;
-                }
-                if (alunos[k].nome.equals(inscricoes[i].aluno)) {
-                   for (l = 0; l < 10; l++) {
-                       if (alunos[k].inscricoes[l] == null) {
-                           alunos[k].inscricoes[l] = inscricoes[i];
-                           break;
-                       }    
-                       // se não tiver espaço não vai botar
-                   } 
-                }
+                if (alunos[k] == null) // limite do vetor
+                    break;
+                if (alunos[k].nome.equals(inscricoes[i].aluno))
+                    for (l = 0; l < 10; l++)
+                        if (alunos[k].inscricoes[l] == null) {
+                            alunos[k].inscricoes[l] = inscricoes[i];
+                            break;
+                        } // se não tiver espaço não vai botar
             }
         }
     }
-    
+
     public static float calcularMedias(Inscricao insc) {
         return (insc.notas[0] + insc.notas[1] + insc.notas[2]) / 3;
     }
-    
+
     public static void imprimirInfoAluno(Aluno alunos) {
         System.out.println("Aluno: " + alunos.nome);
-            System.out.println("CPF: " + alunos.cpf);
-            System.out.println("Matrícula: " + alunos.matricula);
-            System.out.println("Unidade: " + alunos.unidade + "\n");
-            System.out.println("Inscrições: \n");
-            for (int j = 0; j < 10; j++) {
-                if (alunos.inscricoes[j] == null) {
-                    break;
-                }
-                float media = calcularMedias(inscricoes[j]);
-                System.out.println(alunos.inscricoes[j].turma.disciplina.sigla + 
-                        " - " + alunos.inscricoes[j].turma.disciplina.nome + 
-                        " - " + alunos.inscricoes[j].turma.disciplina.codigo);
-                System.out.println("Turma: " + alunos.inscricoes[j].turma.codigo 
-                        + " - " + alunos.inscricoes[j].turma.horario + " - " + 
-                        alunos.inscricoes[j].turma.semestre);
-                System.out.println("Data de matrícula: " + alunos.inscricoes[j].data_matricula
-                + "  Data de cancelamento: " + alunos.inscricoes[j].data_cancelamento);
-                System.out.println("Notas - P1: " + String.format("%.2f", alunos.inscricoes[j].notas[0])
-                + " P2: " + String.format("%.2f", alunos.inscricoes[j].notas[1]) + " P3: "
-                + String.format("%.2f", alunos.inscricoes[j].notas[2]));
-                System.out.println("Media: " + String.format("%.2f", media) + '\n');
-            }
-            System.out.println("---------------------------------------------------\n");
+        System.out.println("CPF: " + alunos.cpf);
+        System.out.println("Matrícula: " + alunos.matricula);
+        System.out.println("Unidade: " + alunos.unidade + "\n");
+        System.out.println("Inscrições: \n");
+        for (int j = 0; j < 10; j++) {
+            if (alunos.inscricoes[j] == null)
+                break;
+            float media = calcularMedias(inscricoes[j]);
+            System.out.println(alunos.inscricoes[j].turma.disciplina.sigla
+                    + " - " + alunos.inscricoes[j].turma.disciplina.nome
+                    + " - " + alunos.inscricoes[j].turma.disciplina.codigo);
+            System.out.println("Turma: " + alunos.inscricoes[j].turma.codigo
+                    + " - " + alunos.inscricoes[j].turma.horario + " - "
+                    + alunos.inscricoes[j].turma.semestre);
+            System.out.println("Data de matrícula: " + alunos.inscricoes[j].data_matricula
+                    + "  Data de cancelamento: " + alunos.inscricoes[j].data_cancelamento);
+            System.out.println("Notas - P1: " + String.format("%.2f", alunos.inscricoes[j].notas[0])
+                    + " P2: " + String.format("%.2f", alunos.inscricoes[j].notas[1]) + " P3: "
+                    + String.format("%.2f", alunos.inscricoes[j].notas[2]));
+            System.out.println("Media: " + String.format("%.2f", media) + '\n');
+        }
+        System.out.println("---------------------------------------------------\n");
     }
-    
+
 }
-
-
-        
-        
-        

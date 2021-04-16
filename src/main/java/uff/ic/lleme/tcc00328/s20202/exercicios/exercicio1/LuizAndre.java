714d8efe5a4558ac91d929720e9986867bff3508
Luiz Leme <lapaesleme@ic.uff.br>
2021-04-16 10:38:31 -0300

    no message

--- src/main/java/uff/ic/lleme/tcc00328/s20202/exercicios/exercicio1/LuizAndre.java
+++
@@ -1,8 +0,0 @@
-package uff.ic.lleme.tcc00328.s20202.exercicios.exercicio1;
-
-public class LuizAndre {
-
-    public static void main(String[] args) {
-        System.out.println("Alô mundo!!!!!!!!!");
-    }
-}
